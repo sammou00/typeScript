22a5af90c0f2994a2297f4fc9effec92a93c3488
samirm00 <sammoham.be@gmail.com>
2024-07-09 09:26:31 +0200

    update setting

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -1,6 +1,7 @@
 // DOM elements
 const title: HTMLElement | null = document.querySelector('.title');
 const message: string = 'Hello World';
-if (title) {
-  title.innerText = message;
+if (title)
+{
+    title.innerText = message;
 }
